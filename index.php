cdfd0ab96eb117feab9b95b570605378823c85d8
Cole R Lawrence <cole@zombiehippie.com>
2013-05-21 14:33:43 -0500

    Establish file hierarchy and linked libraries

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-
+function get_title(){
+    return "Quick Forums";
+}
 ?>
+
+<?php include 'php/header.php'; ?>
+<!-- start index-->
+</head>
+<body>
+
+
+</body>
+<!-- end index  -->
+<?php include 'php/footer.php'; ?>
---
+++ php/header.php
@@ -0,0 +1,10 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title><?php echo get_title();?></title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css"/>
+    <link rel="stylesheet" type="text/css" href="lib/normalize.min.css"/>
+    <script src="lib/zepto.min.js"></script>
